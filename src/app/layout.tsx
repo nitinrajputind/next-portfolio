fa47fb0a71eb7533a5a9a860912cdb00149a99c8
Nitin Rajput <128917704+nitinrajputind@users.noreply.github.com>
2024-07-26 09:07:21 +0530

    header complete and font is added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Raleway, Fira_Code } from "next/font/google";
 import "../scss/index.scss";
 
-const inter = Inter({ subsets: ["latin"] });
+const raleway = Raleway({ subsets: ["latin"] });
+const firaCode = Fira_Code({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
 
 export const metadata: Metadata = {
   title: "Nitin Rajput",
@@ -16,7 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${raleway.className} ${firaCode.className}`}>
+        {children}
+      </body>
     </html>
   );
 }
