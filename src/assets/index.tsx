a099e936dab78eaf752955a2cab3916373745d70
Nitin Rajput <128917704+nitinrajputind@users.noreply.github.com>
2024-08-14 19:41:31 +0530

    fix image import

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,4 +1,24 @@
-import inspironLabs from "./icons/inspironlabs.jpeg";
 import aboutImage from "./image/About.png";
+import inspironLabs from "./icons/inspironlabs.jpeg";
+import blogImage from "./image/Blog.png";
+import blogNodeImage from "./image/BlogNode.png";
+import calculatorImage from "./image/Calculator.png";
+import clockImage from "./image/Clock.png";
+import dictionaryImage from "./image/Dictionary.png";
+import emailTemplateImage from "./image/EmailTemplate.png";
+import eshopImage from "./image/Eshop.png";
+import heroImage from "./image/hero.png";
+import multiFormImage from "./image/multiForm.png";
+import netflixImage from "./image/Netflix.png";
+import nitinBlogImage from "./image/NitinBlog.png";
+import passwordGenrator from "./image/passwordGenrator.png";
+import passwordGenratorImage from "./image/passwordGenrator.png";
+import PortolioImage from "./image/Portolio.png";
+import prepBytesImage from "./image/prepBytes.png";
+import starBucksImage from "./image/StarBucks.png";
+import taskListImage from "./image/TaskList.png";
+import tempratureConverterImage from "./image/TempratureConverter.png";
+import ToDoUIImage from "./image/ToDoUI.png";
+import weatherImage from "./image/weather.png";
 
 export { inspironLabs, aboutImage };
